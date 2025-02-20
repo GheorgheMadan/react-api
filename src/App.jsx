e5b623ec0241a22738655ec34aa5ab49c40ba8c1
Gheorghe <madangheorghe.30@gmail.com>
2025-02-20 14:25:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1></h1>
+      <div className="card">
+        <button>
+        </button>
+      </div>
+    </main>
+  )
+
+}
